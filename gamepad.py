0ecd0fa100327b28a9eabe5894491cf40f32d136
Keegan Neave <keegan.neave@outlook.com>
2020-02-27 22:50:01 +0000

    added debug to categorize the inputs for easier handling

--- gamepad.py
+++ gamepad.py
@@ -3,15 +3,20 @@
 #  Bluetooth pairing via command line:
 #  https://bluedot.readthedocs.io/en/latest/pairpipi.html#using-the-command-line
 
-from evdev import InputDevice, ecodes
+from evdev import InputDevice, ecodes, categorize
 import asyncio
 
 # event3 is specific to my controller, yours may be different
-dev = InputDevice('/dev/input/event3')
+dev = InputDevice('/dev/input/event1')
 
 async def helper(dev):
     async for ev in dev.async_read_loop():
-        print(repr(ev))
+        absevent = categorize(ev)
+
+        axis = ecodes.bytype[absevent.event.type][absevent.event.code]
+        value = absevent.event.value
+        print(axis, value)
+
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(helper(dev))
